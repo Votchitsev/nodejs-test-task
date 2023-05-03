85344ace833219f8ea33ae069fa9af5e59912f0d
Dmitry <dmitry.votchitsev@gmail.com>
2023-05-03 10:25:43 +0200

    ПОдключил базу данных

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
-
 const express = require('express');
+const sequelize = require('./database');
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('App started');
+app.get('/', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.send('OK');
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = app;
