6edb63c516c5a873d1e02cc7d7d7d4640fb228d8
Dmitry <dmitry.votchitsev@gmail.com>
2023-05-05 15:46:50 +0200

    Изменил app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const sequelize = require('./database');
 const auth = require('./routes/auth');
 const post = require('./routes/post');
 const file = require('./routes/file');
@@ -16,7 +15,6 @@ app.use('/file', file);
 
 app.get('/', async (req, res) => {
   res.sendFile(path.join(__dirname, 'templates/about.html'));
-  await sequelize.sync({ force: true });
 });
 
 module.exports = app;
