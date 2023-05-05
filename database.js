45b7b8cd7b0eff3ebe524d70949681a05a9f7a1a
Dmitry <dmitry.votchitsev@gmail.com>
2023-05-05 12:34:23 +0200

    Обновление базы данных при запуске

--- database.js
+++ database.js
@@ -2,4 +2,10 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URI);
 
+async function syncDatabase() {
+  await sequelize.sync({ alter: true });
+}
+
+syncDatabase();
+
 module.exports = sequelize;
